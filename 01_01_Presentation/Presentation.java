106405adfa5b9734f38ac7dabde7dfc0a3a62e54
Patricia <patriciabarbod03@gmail.com>
2026-09-21 12:17:47 +0200

    Exercici presentacio fet

---
+++ 01_01_Presentation/Presentation.java
@@ -0,0 +1,16 @@
+public class Presentation {
+    public static void main(String[] args) {
+
+        String name = "John";
+        String surname = "Doe";
+        int age = 33;
+        String address = "123 First Avenue, London, WC1 6HB";
+        double height =  1.82;
+
+        System.out.println("Hello! My name is " + name + " " + surname + ".");
+        System.out.println("I am " + age + " y/o" + ".");
+        System.out.println("My address is " + address + ".");
+        System.out.println("My height is " + height + " meters.");
+ 
+    }
+}

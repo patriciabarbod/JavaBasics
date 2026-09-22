50563ddce9d4df8e49e376b00d3ebd0ca87ba741
Patricia <patriciabarbod03@gmail.com>
2026-09-22 13:04:46 +0200

    exercici hores fet

---
+++ 01_04_Hours2Seconds/Hours2Seconds.java
@@ -0,0 +1,18 @@
+public class Hours2Seconds{
+
+	public static void main(String[] args) {
+		int hours = 12;
+		int seconds = hours * 3600;
+		System.out.println(hours + " hours are " + seconds + " seconds");
+
+        hours = 15;
+		seconds = hours * 3600;
+		System.out.println(hours + " hours are " + seconds + " seconds");
+
+		hours = 23;
+		seconds = hours * 3600;
+		System.out.println(hours + " hours are " + seconds + " seconds");
+
+    }
+
+}

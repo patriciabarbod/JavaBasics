2f8779340291c3bf520b3ca5f02cd5fc637da5bc
Patricia <patriciabarbod03@gmail.com>
2026-09-22 12:50:06 +0200

    exercici de potencia fet

---
+++ 01_03_PowerFormula/PowerFormula.java
@@ -0,0 +1,11 @@
+public class PowerFormula {
+
+	public static void main(String[] args) {
+		double force = 125;
+		double distance = 37;
+		double time = 12;
+	
+		double power = force * distance / time;
+		System.out.println("Power = " + power + " W");		 
+	}
+}
